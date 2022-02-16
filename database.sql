a4169033a38e32587d1c42c2850e99714247a649
Tom Lengkeek <330713@student.mboutrecht.nl>
2022-02-16 15:59:04 +0100

    intitial commit with database

---
+++ database.sql
@@ -0,0 +1,22 @@
+/* drop database Magazijn; */
+
+create database Magazijn;
+use Magazijn;
+
+create table Category (
+	category varchar(255) not null,
+    PRIMARY KEY (category)
+);
+
+create table Artikel (
+		artikelid int not null AUTO_INCREMENT,
+        omschrijving varchar(255) not null,
+        category varchar(255) not null,
+        aantal int not null,
+        kosten double,
+        locatie varchar(255),
+        PRIMARY KEY (artikelid),
+        FOREIGN KEY (category) REFERENCES Category(category)
+        
+ );
+ 
