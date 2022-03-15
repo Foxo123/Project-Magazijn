cc598d4d23c451f12b21ebf4839a9b4f28a5d7d8
Fabiandejong07 <81682500+Fabiandejong07@users.noreply.github.com>
2022-03-15 15:30:48 +0100

    magaijnen overzicht en goedkeuren toegevoegd

---
+++ magazijn_git/artikelGoedkeuren.php
@@ -0,0 +1,67 @@
+<?php
+require_once './class.php';
+//class artikel goedkeuren voor de opmaak?
+  class ArtikelGoedkeuren{
+   
+    public $conn;
+
+// here you connect with the database
+    public function __construct(){ 
+
+      require_once "../Database.php";
+      $database = new Database();
+
+      $this->conn = $database->conn;
+    }
+    public function read(){
+        try{
+        
+           //here select the tabel from the database
+            $sql = $this->conn->prepare("SELECT * FROM artikel");
+            
+            $sql->execute();
+    
+            $result = $sql->setFetchMode(PDO::FETCH_OBJ);
+    
+            $records = "";
+            //here you make te card with the informatien
+            while($record = $sql->fetch()){
+                $records .= "    <div class='card' style='width: 18rem;'>
+                <div class='card-header1'>
+                  Featured
+                </div>
+                <ul class='list-group list-group-flush'>
+                <p  class='card-text'>" . $record->omschrijving. "</p>
+                <p  class='card-text'>" . $record->category . "</p>
+                <p  class='card-text'>" . $record->aantal . "</p>
+               
+                <p class='card-text'>" . $record->inkoopprijs . "</p>
+                 
+
+                </ul>
+              </div>
+ 
+             
+
+              <button type='button' class='btn btn-success' href='./Goedkeurenafkeuren.php? id='>goedkeuren" . $record->id . "</button>
+              <button type'button' class='btn btn-danger' href='Goedkeuren.php?id=$record->id'>afkeuren". $record->inkoopprijs .    " </button>
+              
+              
+            ";}
+           
+            return $records;
+            
+        }catch(PDOException $e){
+          //this is for the errors
+            array_push($this->logs,"reading failed" . $e->getMessage());
+          
+        }
+    }
+      }
+      
+    ?>
+
+
+
+
+               
